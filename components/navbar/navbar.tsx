aeb679f848a4d61e2e44046e4a5e2b5f55335dd2
Hermann <hermanrazafinbdranaivo@gmail.com>
2024-08-15 14:18:10 +0300

    correction de bug

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -14,8 +14,8 @@ const Navbar: React.FC = async () => {
       link.addEventListener('click', (event) => {
         event.preventDefault();
 
-        const targetId = link.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
+        const targetId = link.getAttribute('href')?.substring(1);
+        const targetElement = document.getElementById(targetId ?? "");
 
         if (targetElement) {
           targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
