244c5895a42b02e3d715cff61cbcc50f41e2df29
Hermann <hermanrazafinbdranaivo@gmail.com>
2024-11-21 10:47:00 +0300

    fix bug in FAQ component

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -12,43 +12,48 @@ interface FAQProps {
 }
 
 const FAQList: FAQProps[] = [
-  
   {
     question: "Quels services proposez-vous ?",
-    answer: "Je propose des solutions sur mesure pour répondre à vos besoins numériques : <ul>"+
-    "<li>Développement de sites web modernes (vitrines, e-commerce, etc.).</li>" +
-    "<li>Développement d'applications mobiles</li>"+
-    "<li>Maintenance et optimisation de sites existants</li>"+
-    "<li>Intégration d’outils digitaux adaptés à votre entreprise</li>"+
-    "<li>Conception UX/UI pour une expérience utilisateur optimale.</li></ul>",
+    answer:
+      "Je propose des solutions sur mesure pour répondre à vos besoins numériques : <ul>" +
+      "<li>Développement de sites web modernes (vitrines, e-commerce, etc.).</li>" +
+      "<li>Développement d&apos;applications mobiles</li>" +
+      "<li>Maintenance et optimisation de sites existants</li>" +
+      "<li>Intégration d&apos;outils digitaux adaptés à votre entreprise</li>" +
+      "<li>Conception UX/UI pour une expérience utilisateur optimale.</li></ul>",
     value: "item-1",
   },
   {
     question: "Nos services sont-ils adaptés aux petites entreprises ?",
-    answer: "Oui ! Nous travaillons avec des entreprises de toutes tailles, des start-ups aux grandes organisations.",
+    answer:
+      "Oui ! Nous travaillons avec des entreprises de toutes tailles, des start-ups aux grandes organisations.",
     value: "item-2",
   },
   {
     question: "Travaillez-vous uniquement avec des entreprises ou aussi avec des particuliers ?",
-    answer: "Je travaille principalement avec des entreprises (TPE, PME, start-ups, grandes organisations), mais je peux également collaborer avec des particuliers ayant des projets numériques concrets.",
+    answer:
+      "Je travaille principalement avec des entreprises (TPE, PME, start-ups, grandes organisations), mais je peux également collaborer avec des particuliers ayant des projets numériques concrets.",
     value: "item-3",
   },
   {
     question: "Quels sont vos délais de réalisation ?",
-    answer: "Les délais dépendent de la complexité de votre projet. Par exemple : <ul>" +
+    answer:
+      "Les délais dépendent de la complexité de votre projet. Par exemple : <ul>" +
       "<li>Un site vitrine peut être livré en 2 à 4 semaines.</li>" +
       "<li>Une boutique en ligne ou une application nécessite généralement 4 à 8 semaines.</li>" +
-      "<li>Je m'engage toujours à respecter les échéances définies ensemble.</li></ul>",
+      "<li>Je m&apos;engage toujours à respecter les échéances définies ensemble.</li></ul>",
     value: "item-4",
   },
   {
     question: "Offrez-vous une garantie après livraison ?",
-    answer: "Oui, une garantie de suivi est incluse après la livraison pour résoudre les éventuels bugs ou ajustements nécessaires. La durée de cette garantie dépend du type de projet.",
+    answer:
+      "Oui, une garantie de suivi est incluse après la livraison pour résoudre les éventuels bugs ou ajustements nécessaires. La durée de cette garantie dépend du type de projet.",
     value: "item-5",
   },
   {
     question: "Quels outils et technologies utilisez-vous ?",
-    answer: "Je maîtrise les technologies modernes pour garantir des solutions performantes :<ul>" +
+    answer:
+      "Je maîtrise les technologies modernes pour garantir des solutions performantes :<ul>" +
       "<li>Développement Frontend : React.js, Next.js.</li>" +
       "<li>Développement Backend : Node.js, AdonisJS, Flask.</li>" +
       "<li>CMS : WordPress, Shopify.</li>" +
@@ -57,6 +62,7 @@ const FAQList: FAQProps[] = [
     value: "item-6",
   },
 ];
+
 
 export const FAQ = () => {
   return (
